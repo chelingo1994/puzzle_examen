b8eca399a2dcced7307c5a58368d25b89ac30473
justi <justi@DESKTOP-GLE8U2B>
2026-09-05 22:25:57 -0400

    home 5/09/2026

--- app/src/main/java/bo/edu/uajms/marcelojustiniano/puzzle_examen/mainActivity.kt
+++ app/src/main/java/bo/edu/uajms/marcelojustiniano/puzzle_examen/mainActivity.kt
@@ -11,6 +11,8 @@ class mainActivity:AppCompatActivity(){
     //Controles
     private lateinit var BTNtablero: Array<Button>
     private lateinit var btnRestart: Button
+    private lateinit var btnRandom: Button
+    private lateinit var btnVerify: Button
     private lateinit var tablero: Array<Array<String>>
     //Otras variables
     private var rows=4;
@@ -19,7 +21,50 @@ class mainActivity:AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activitymain)
+        BTNtablero=arrayOf(
+            findViewById(R.id.btn01),
+            findViewById(R.id.btn02),
+            findViewById(R.id.btn03),
+            findViewById(R.id.btn04),
+            findViewById(R.id.btn05),
+            findViewById(R.id.btn06),
+            findViewById(R.id.btn07),
+            findViewById(R.id.btn08),
+            findViewById(R.id.btn09),
+            findViewById(R.id.btn10),
+            findViewById(R.id.btn11),
+            findViewById(R.id.btn12),
+            findViewById(R.id.btn13),
+            findViewById(R.id.btn14),
+            findViewById(R.id.btn15),
+            findViewById(R.id.btn16)
 
+        )
+
+        btnRestart=findViewById(R.id.btnRestart)
+        btnRandom=findViewById(R.id.btnRandomize)
+        btnVerify=findViewById(R.id.btnVerify)
+        tablero= Array(rows){ Array(cols){""} }
+        var numero = 1
+
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                if (numero <= 15) {
+                    tablero[i][j] = numero.toString()
+                    numero++
+                } else {
+                    tablero[i][j] = ""
+                }
+            }
+        }
+        var posicion = 0
+
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                BTNtablero[posicion].text = tablero[i][j]
+                posicion++
+            }
+        }
 
     }
 }
